e6b4446e7c4ebda56b2837c5c990ccbdfd73c6fb
Giovanna Peres <150537182+gi-peres@users.noreply.github.com>
2026-06-02 21:37:49 -0300

    Remove firebase_options.dart import and use default Firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_ai/firebase_ai.dart';
-import 'firebase_options.dart';
 import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
